487b87e05884a3f4444284894dec368371f2b058
Diego Sanhueza <diegocontacto1996@gmail.com>
2023-06-08 15:36:25 -0400

    audios cargados

--- client/src/data/data.jsx
+++ client/src/data/data.jsx
@@ -7,6 +7,17 @@ import plaza from "./../assets/img/locations/plaza.jpg"
 
 import audio_1 from "./../assets/audio/idea_1.mp3"
 import audio_2 from "./../assets/audio/idea_2.mp3"
+import TUNELPTADEPARRAMEZCLADO from "./../assets/audio/TUNELPTADEPARRAMEZCLADO.wav"
+import LOSTILOSMEZCLADO from "./../assets/audio/LOSTILOSMEZCLADO.wav"
+import PLAYABELLAVISTAMEZCLADO from "./../assets/audio/PLAYABELLAVISTAMEZCLADO.wav"
+import PLAYAPTADEPARRAMEZCLADO from "./../assets/audio/PLAYAPTADEPARRAMEZCLADO.wav"
+import CASADEPIEDRAMEZCLADO from "./../assets/audio/CASADEPIEDRAMEZCLADO.wav"
+import ESQUINAREDONDAMEZCLADO from "./../assets/audio/ESQUINAREDONDAMEZCLADO.wav"
+import EXPLANADAMEZCLADO from "./../assets/audio/EXPLANADAMEZCLADO.wav"
+import PLAYALOSMORROSMEZCLA from "./../assets/audio/PLAYALOSMORROSMEZCLA.wav"
+import PLAZATOMEMEZCLA from "./../assets/audio/PLAZATOMEMEZCLA.wav"
+import TUNELCENTENARIOMEZCLA from "./../assets/audio/TUNELCENTENARIOMEZCLA.wav"
+
 
 
 export const initialMarkers = [
@@ -18,7 +29,7 @@ export const initialMarkers = [
         label: { color: "white", text: "Casa de Piedra" },
         draggable: false,
         image: casaPiedra,
-        audio: audio_1,
+        audio: CASADEPIEDRAMEZCLADO,
         titulo: "Casa de Piedra",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla"
     },
@@ -30,7 +41,7 @@ export const initialMarkers = [
         label: { color: "white", text: "Esquina Redonda" },
         draggable: false,
         image: esquina,
-        audio: audio_2,
+        audio: ESQUINAREDONDAMEZCLADO,
         titulo: "Esquina Redonda",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla"
     },
@@ -42,7 +53,7 @@ export const initialMarkers = [
         label: { color: "white", text: "Explanada" },
         draggable: false,
         image: explanada,
-        audio: audio_1,
+        audio: EXPLANADAMEZCLADO,
         titulo: "Explanada",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla"
     },
@@ -54,7 +65,7 @@ export const initialMarkers = [
         label: { color: "white", text: "Los Morros" },
         draggable: false,
         image: morro,
-        audio: audio_2,
+        audio: PLAYALOSMORROSMEZCLA,
         titulo: "Los Morros",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla"
     },
@@ -66,7 +77,7 @@ export const initialMarkers = [
         label: { color: "white", text: "Plaza de Tomé" },
         draggable: false,
         image: plaza,
-        audio: audio_1,
+        audio: PLAZATOMEMEZCLA,
         titulo: "Plaza de Tomé",
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla"
     },
